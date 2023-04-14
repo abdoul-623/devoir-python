c9ea45c0e38df4fd0e371b7955336c63201b392f
abdoul-623 <129530432+abdoul-623@users.noreply.github.com>
2023-04-14 17:15:16 +0200

    Create devoir5.py

---
+++ devoir5.py
@@ -0,0 +1,13 @@
+class Employer :
+    pass
+Personne = Employer()
+
+Personne.nom = 'Camara'
+Personne.prenom = 'Abdoul Kader'
+Personne.age = 21
+Personne.filiation = 'Salif et Mariame Sylla'
+Personne.sexe = 'M'
+Personne.taille = 1.84
+Personne.poste = 'Membre'
+Personne.experience = 'Développeur'
+
